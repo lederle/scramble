81fe91207b2bd99935b7a93db1940aecc18bbd75
Mike Lederle <mrlederle@gmail.com>
2025-09-09 13:04:49 -0400

    Final analysis

--- scramble.py
+++ scramble.py
@@ -3,13 +3,26 @@ from itertools import combinations
 from pprint import pprint
 import pandas as pd
 
-score_dist = pd.read_csv("score_dist_no_sub.csv", index_col="Name")
+score_dist = pd.read_csv("score_dist_scramble.csv", index_col="Name")
 score_dist["par_or_better_percentage"] =  score_dist[["Eagle or better", "Birdie", "Par"]].sum(axis = 1) / score_dist.sum(axis = 1)
 score_dist['par_or_better_percentage'] = score_dist['par_or_better_percentage'] * 100
-player_index = [10.8, 8.9, 7.1, 13.3, 7.1, 13.3, 8.1, 0.2, 21.5, 11.7, 14.7, 11.7, 5.4, 3.9, 11.0, 9.8, 7.9, 16.2, 9.8, 6.0, 6.9, 8.2]
-score_dist['HIndex'] = player_index
 player_list = list(score_dist.index)
-# TODO: make final roster (eliminate subs)
+score_dist["Total Holes"] = score_dist.drop(["HIndex", "par_or_better_percentage"], axis = 1).sum(axis = 1)
+pprint(score_dist[["HIndex", "par_or_better_percentage"]])
+pprint(score_dist[["Eagle or better", "Birdie", "Par", "Total Holes", "par_or_better_percentage"]].rename(columns = {"par_or_better_percentage": "POB_percentage", "Eagle or better": "Eagle"}))
+print("==================================================")
+team_str = [
+    ['Lederle, Mike', 'Wreford, Nick', 'Leighton, Tim', 'Lederle, Saeha'],
+    ['Palmer, Bobby', 'Koenig, Zach', 'ramsey, jim', 'Hojnacki, Max'],
+    ['Wreford, Bob', 'Rhodehouse, Jason', 'Borrego, Nick', 'West, Travis'],
+    ['Gamble, Tim', 'Cindric, Mike', 'Orsette, Rich', 'Clem, Wally'],
+    ['Cunningham, John', 'Taylor, Dorian', 'Roberts, Thomas', 'Orsette, Antony']
+]
+for team in team_str:
+    print(f"TEAM: {"; ".join(team)}")
+    print(f"INDEX TOTAL: {round(float(score_dist[['HIndex']].loc[team].sum().iloc[0]), 2)}")
+    print(f"PAR BREAKER, TEAM AVERAGE {round(float(score_dist[['par_or_better_percentage']].loc[team].sum().iloc[0]) / 4, 2)}")
+    print("--------------------------------------------------")
 
 Player = namedtuple("Player", "id name hindex par_or_better")
 player_set = set()
@@ -35,19 +48,19 @@ for i, p_tup in enumerate(all_teams):
         index_sum += p.hindex
     team_indices.append((i, round(index_sum, 2)))
     
-print("HANDICAP INDEX SECTION")
+#print("HANDICAP INDEX SECTION")
 sorted_indices = sorted(team_indices, key = lambda x: x[1])
-print(sorted_indices[len(sorted_indices) // 2])
+#print(sorted_indices[len(sorted_indices) // 2])
 n = len(sorted_indices)
-print(f"average: {sum(j[1] for j in sorted_indices) / n}")
-print(f"length: {n}")
+#print(f"average: {sum(j[1] for j in sorted_indices) / n}")
+#print(f"length: {n}")
 #print(sorted_indices[((n-50)//2):((n+50)//2)])
-median_teams = list(filter(lambda x: x[1] == 38.5, sorted_indices))
-print(median_teams)
-median_teams_idx = [i[0] for i in median_teams]
-print(median_teams_idx)
-median_team_rosters = [all_teams[i] for i in median_teams_idx]
-pprint(median_team_rosters[:10])
+#median_teams = list(filter(lambda x: x[1] == 38.5, sorted_indices))
+#print(median_teams)
+#median_teams_idx = [i[0] for i in median_teams]
+#print(median_teams_idx)
+#median_team_rosters = [all_teams[i] for i in median_teams_idx]
+#pprint(median_team_rosters[:10])
 
 # PAR OR BETTER FOR EACH TEAM
 team_indices = []
@@ -57,16 +70,16 @@ for i, p_tup in enumerate(all_teams):
         par_breaker_sum += p.par_or_better
     team_indices.append((i, round(par_breaker_sum, 2)))
     
-print("PAR BREAKER SECTION")
+#print("PAR BREAKER SECTION")
 sorted_indices = sorted(team_indices, key = lambda x: x[1])
-print(sorted_indices[len(sorted_indices) // 2])
+#print(sorted_indices[len(sorted_indices) // 2])
 n = len(sorted_indices)
-print(f"average: {sum(j[1] for j in sorted_indices) / n}")
-print(f"length: {n}")
-median_teams = list(filter(lambda x: x[1] == 95, sorted_indices))
-print(median_teams)
-median_teams_idx = [i[0] for i in median_teams]
-print(median_teams_idx)
-median_team_rosters = [all_teams[i] for i in median_teams_idx]
-pprint(median_team_rosters)
+#print(f"average: {sum(j[1] for j in sorted_indices) / n}")
+#print(f"length: {n}")
+#median_teams = list(filter(lambda x: x[1] == 95, sorted_indices))
+#print(median_teams)
+#median_teams_idx = [i[0] for i in median_teams]
+#print(median_teams_idx)
+#median_team_rosters = [all_teams[i] for i in median_teams_idx]
+#pprint(median_team_rosters)
 print("OK")
